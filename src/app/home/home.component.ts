32dc94239987675d0346d24c43f39cd004edd10d
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2022-06-28 13:15:01 -0400

    mvp for todo list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+todoCount: number = 4;
+btnTxt: string = 'Add';
+ToDoTxt: string = 'New Todo'
+todos: any = [];
+
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.todoCount = this.todos.length;
+  }
+
+  addItem() {
+    this.todos.push(this.ToDoTxt);
+    this.ToDoTxt ="";
+    this.todoCount = this.todos.length;
   }
 
 }
