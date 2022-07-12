848646cf4cf5d1e929a223967021b96159b3621e
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2022-07-12 19:45:31 -0400

    colour coding for individual todos working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,13 +14,21 @@ btnTxt: string = 'Add';
 ToDoTxt: string = ''
 todos: any = [];
 color: string = "white";
+clicked: {[key: number]: string} = {};
+// this.todos = this.cookie.get('todosList');
+
 
 
 
   constructor(private cookie: CookieService) {
 
+    console.log(this.todos)
+    console.log((this.cookie.get('todosList')))
+    console.log( )
 
-   }
+  }
+
+
 
   ngOnInit() {
     this.todoCount = this.todos.length;
@@ -28,7 +37,7 @@ color: string = "white";
   addItem() {
     if (this.ToDoTxt !== "" ) {
       this.todos.push(this.ToDoTxt);
-      this.cookie.set("l", "todos")
+      this.cookie.set("todosList", JSON.stringify(this.todos))
       this.ToDoTxt ="";
       this.todoCount = this.todos.length;
     }
@@ -38,16 +47,24 @@ color: string = "white";
 
   deleteItem(i: number) {
     this.todos.splice(i, 1);
+    this.cookie.set("todos", this.todos)
   }
 
   itemStatus(i: number) {
-    if (this.color === "salmon" ) {
-      this.color = "white";
-    } else if (this.color === "white") {
-      this.color = "limegreen";
+    if (this.clicked[i] === "salmon") {
+      this.clicked[i] = "white";
+    } else if (this.clicked[i] === "white") {
+      this.clicked[i] = "limegreen";
     } else {
-      this.color = "salmon";
+      this.clicked[i] = "salmon";
     }
+    // if (this.color === "salmon" ) {
+    //   this.color = "white";
+    // } else if (this.color === "white") {
+    //   this.color = "limegreen";
+    // } else {
+    //   this.color = "salmon";
+    // }
   }
 
 }
